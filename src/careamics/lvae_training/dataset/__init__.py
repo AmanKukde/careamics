6053dc6468cceece0aa48d6aab69f38aafa23c12
Aman Kukde <aman.kukde@external.fht.org>
2025-11-04 15:24:08 +0100

    Added stitching functions

--- src/careamics/lvae_training/dataset/__init__.py
+++ src/careamics/lvae_training/dataset/__init__.py
@@ -5,10 +5,6 @@ from .multich_dataset import MultiChDloader
 from .multicrop_dset import MultiCropDset
 from .multifile_dataset import MultiFileDset
 from .types import DataSplitType, DataType, TilingMode
-# from .windowed_tiling_dataset import WindowedTilingDloader, WindowedLCDLoader
-from .windowed_tiling_dataset import WindowedTilingDloader
-from .windowed_lc_dataset import WindowedLCDLoader
-
 
 __all__ = [
     "DatasetConfig",
@@ -21,6 +17,4 @@ __all__ = [
     "DataType",
     "DataSplitType",
     "TilingMode",
-    "WindowedTilingDloader",
-    "WindowedLCDLoader"
 ]
